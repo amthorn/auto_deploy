1383ff8497be044d8a1b71a70138700e9196a0cd
Ava Thorn <avthorn@cisco.com>
2020-01-28 00:49:32 +0100

    more stuff

--- pull_and_deploy.sh
+++ pull_and_deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[[ $(/usr/bin/git pull origin master) =~ "Already up to date." ]] && /deploy/deploy.sh
+[[ $(/usr/bin/git pull origin master) =~ "Already up to date." ]] || /deploy/deploy.sh
